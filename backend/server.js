e1e198dffffae3c3eb9a12e7d06a99a104d4739f
Matej Maťus <matej.matus01@gmail.com>
2024-12-02 08:19:36 +0100

    test

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,6 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-
-
-
 // Database stuff
 mongoose
   .connect(process.env.MONGO_URI)
@@ -34,15 +31,17 @@ mongoose.connection.once("open", () =>
 // const __dirname  = path.resolve()
 console.log(__dirname)
 
-if(process.env.MODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")))
+if (process.env.MODE_ENV === "production") {
+  // app.use(express.static(path.join(__dirname, "/frontend/dist")))
+  app.use(express.static("/home/matej/tulenserver/tulenfixed/frontend/dist"))
 
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  // })
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    res.sendFile("/home/matej/tulenserver/tulenfixed/frontend/dist/index.html")
   })
-  
 }
-
 
 const articleRouter = require("./routes/articles")
 app.use("/articles", articleRouter)
