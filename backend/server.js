e6eff0f411bb1d6bbfd287bbe0d71297ccfa7713
Matej Maťus <matej.matus01@gmail.com>
2024-12-12 10:02:20 +0100

    afads

--- backend/server.js
+++ backend/server.js
@@ -9,15 +9,15 @@ require("dotenv").config()
 const app = express()
 const port = process.env.PORT || 5000
 
-// app.use(cors())
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors())
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 app.use(express.json())
 
 const articleRouter = require("./routes/articles")
