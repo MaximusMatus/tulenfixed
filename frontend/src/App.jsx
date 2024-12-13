14cca7dd979b25a47184830894cce3ec50a08dc7
Matej Maťus <matej.matus01@gmail.com>
2024-12-13 09:12:03 +0100

    repair

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,8 +8,8 @@ import AddArticle from "./components/AddArticle"
 import EditArticle from "./components/EditArticle"
 import PreviewArticle from "./components/PreviewArticle"
 import Article from "./components/Article"
-import axios from 'axios'
-import {Routes, Route} from 'react-router-dom'
+import axios from "axios"
+import { Routes, Route } from "react-router-dom"
 
 // const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT
 // const API_ENDPOINT = 'http://localhost:5000/articles'
@@ -17,31 +17,28 @@ import {Routes, Route} from 'react-router-dom'
 function App() {
   const [posts, setPosts] = useState([])
   useEffect(() => {
-    axios.get("/articles")
-    .then(res => {setPosts(res.data)
-       console.log(`${API_ENDPOINT}`)})
-    .catch(error => console.log(error))
-  },[])
+    axios
+      .get("/articles")
+      .then((res) => {
+        setPosts(res.data)
+      })
+      .catch((error) => console.log(error))
+  }, [])
   return (
     <div className="body-wrapper">
       <Header />
       <div className="content-wrapper">
-
-      <Routes>
-        <Route path='/admin' element={<Admin/>}/>
-        <Route path='/admin/add' element={<AddArticle/>}/>
-        <Route path='/admin/:id' element={<EditArticle/>}/>
-        <Route path='/admin/preview/:id' element={<Article/>}/>
-        <Route path='/' element={<MainSection posts={posts}/>}/>
-        <Route path='/article/:id' element={<Article/>}/>
-        
-        
-      </Routes>
-
+        <Routes>
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/admin/add" element={<AddArticle />} />
+          <Route path="/admin/:id" element={<EditArticle />} />
+          <Route path="/admin/preview/:id" element={<Article />} />
+          <Route path="/" element={<MainSection posts={posts} />} />
+          <Route path="/article/:id" element={<Article />} />
+        </Routes>
       </div>
-      <Footer/>
+      <Footer />
     </div>
-
   )
 }
 
