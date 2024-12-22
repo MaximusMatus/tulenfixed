4fd7909a27407ff6a694d5443491a24a6d58be8a
Matej Maťus <matej.matus01@gmail.com>
2024-12-22 11:20:14 +0100

    asdfasdf

--- backend/server.js
+++ backend/server.js
@@ -10,14 +10,7 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.use(cors())
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+
 app.use(express.json())
 
 const articleRouter = require("./routes/articles")
@@ -41,7 +34,7 @@ mongoose.connection.once("open", () =>
 
 if (process.env.MODE_ENV === "production") {
   // app.use(express.static(path.join(__dirname, "/frontend/dist")))
-  // app.use(express.static("/home/matej/tulenserver/tulenfixed/frontend/dist"))
+  app.use(express.static("/home/matej/tulenserver/tulenfixed/frontend/dist"))
 // app.use("/uploads", express.static("/home/matej/tulenserver"))
   // app.get("*", (req, res) => {
   //   res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
